797ca76a174e71d96bc939d7cc1c8ee557dfcc95
Mango-Juice <wjsalsrb5@naver.com>
2025-06-07 17:25:41 +0900

    Feat: 시설 관련 API 라우터 추가

--- app/api/main.py
+++ app/api/main.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.routes import auth, user, survey, map
+from app.api.routes import auth, user, survey, map, facility
 
 api_router = APIRouter()
 api_router.include_router(auth.router)
 api_router.include_router(user.router)
 api_router.include_router(survey.router)
 api_router.include_router(map.router)
+api_router.include_router(facility.router)
